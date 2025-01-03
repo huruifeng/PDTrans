cbf87df422b334248cfcab0a006957c2aa937855
huruifeng <huruifeng.cn@hotmail.com>
2025-01-02 22:58:43 -0500

    update

--- src/22_model_D_Transformer.py
+++ src/22_model_D_Transformer.py
@@ -2,6 +2,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from matplotlib import pyplot as plt
 from scipy.stats import pearsonr
 from sklearn.metrics import r2_score
 from torch.utils.data import DataLoader, Dataset
@@ -29,7 +30,7 @@ class UPDRSDataset(Dataset):
 
 # Define the Transformer model
 class TransformerPredictor(nn.Module):
-    def __init__(self, num_genes, hidden_size=1024, num_heads=4, num_layers=6, dropout=0.1):
+    def __init__(self, num_genes, hidden_size=128, num_heads=4, num_layers=6, dropout=0.1):
         super(TransformerPredictor, self).__init__()
         # Transformer encoder layers
         gene_encoder_layer = nn.TransformerEncoderLayer(d_model=hidden_size, nhead=num_heads, dropout=dropout)
@@ -136,6 +137,43 @@ def train_model(model, train_loader, val_loader=None, num_epochs=30, learning_ra
 
     print("Training completed.")
 
+
+## Test the model
+def test_model(model, test_dataloader, device=torch.device("cuda" if torch.cuda.is_available() else "cpu")):
+    model.to(device)
+    model.eval()
+    criterion = nn.MSELoss()
+
+    total_loss = 0.0
+
+    print("Testing...")
+
+    run_len = len(test_dataloader) // 10 + 1
+
+    test_results_df = pd.DataFrame(columns=["Actual", "Predicted"])
+    with torch.no_grad():
+        for i,(genes_previous, updrs_previous, genes_current, updrs_current, updrs_next) in enumerate(test_dataloader,0):
+            genes_previous, updrs_previous = genes_previous.to(device), updrs_previous.to(device)
+            genes_current, updrs_current = genes_current.to(device), updrs_current.to(device)
+            updrs_next = updrs_next.to(device)
+
+            outputs = model(genes_current, updrs_current, genes_previous, updrs_previous)
+            loss = criterion(outputs, updrs_next)
+            total_loss += loss.item()
+
+            test_results_df = pd.concat([test_results_df, pd.DataFrame(
+                {"Actual": updrs_next.cpu().numpy(), "Predicted": outputs.squeeze().cpu().numpy()})], ignore_index=True)
+
+            if i % run_len == 0:
+                print("#", end="", flush=True)
+
+    print("")
+
+    print(f"Test Loss: {total_loss / len(test_dataloader):>.4f}")
+    print("Test completed.")
+
+    return test_results_df
+
 # Example usage
 if __name__ == "__main__":
     # Load and preprocess data
@@ -158,7 +196,7 @@ if __name__ == "__main__":
 
     val_dataset = UPDRSDataset(gene_expression_t1.loc[val_index,:].values, updrs_t1[val_index].values,
                                gene_expression_t2.loc[val_index,:].values, updrs_t2[val_index].values, updrs_t3[val_index].values)
-    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=True)
 
     # Initialize and train the model
     num_genes = 874
@@ -170,3 +208,41 @@ if __name__ == "__main__":
 
     # Save the model
     torch.save(model.state_dict(), '../models/model_D_Transformer_model.pt')
+
+    ## Test the model
+    test_df = pd.read_csv("../results/training_testing/PPMI_data_previous_current_next.csv", index_col=0)
+    test_df = test_df.dropna(axis=0)
+
+    gene_expression_t1 = test_df.loc[:, test_df.columns.str.startswith('previous_ENSG')]
+    gene_expression_t2 = test_df.loc[:, test_df.columns.str.startswith('current_ENSG')]
+    updrs_t1 = test_df['previous_updrs']
+    updrs_t2 = test_df['current_updrs']
+    updrs_t3 = test_df['next_updrs']
+
+    test_dataset = UPDRSDataset(gene_expression_t1.values, updrs_t1.values,
+                               gene_expression_t2.values, updrs_t2.values, updrs_t3.values)
+    test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+
+    model = TransformerPredictor(num_genes)
+    model.load_state_dict(torch.load('../models/model_D_Transformer_model.pt'))
+    model.to(device)
+
+    test_results_df = test_model(model, test_dataloader, device)
+
+    ## Calculate R^2
+    r2 = r2_score(test_results_df["Actual"], test_results_df["Predicted"])
+    pcc = pearsonr(test_results_df["Actual"], test_results_df["Predicted"])[0]
+    print(f"R^2 score: {r2:.4f}, PCC: {pcc:.4f}")
+
+    # %%
+    ## plot actual vs predicted
+    plt.scatter(test_results_df["Actual"], test_results_df["Predicted"])
+    plt.xlabel("Actual")
+    plt.ylabel("Predicted")
+    plt.title("Actual vs Predicted")
+    plt.show()
+
+    ## Save the test results
+    test_results_df.to_csv("../results/training_testing/PDBP_test_results.csv")
+    print("Test results saved.")
+
